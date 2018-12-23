9590f2975084761ab57249d3a6c796f06aa6d259
Edward Krayer <github@edwardkrayer.com>
2018-12-22 17:21:20 -0800

    File cleanup.

--- build_latest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build latest
-docker build --no-cache --tag=dxvk/dxvk-builder:latest - < Dockerfile.latest
-mkdir -p ~/dxvk-build/latest
-export DXVK_DIR=~/dxvk-build/latest
-docker run --volume=$DXVK_DIR:/root/bin/ --tty dxvk/dxvk-builder:latest
